9edc070e2c8b95fce061d68e96d8f89f4071c52d
Anton718 <anton.kostroma@yahoo.com>
2023-04-09 19:44:56 -0400

    add default

--- washing_machine.c
+++ washing_machine.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+
 
 int main()
 {
@@ -7,12 +11,14 @@ int main()
     int programm;
     int additional;
 
+
+
     printf("\nStart 1/ Off - 0\n");
     scanf("%d", &start);
     
     if (start == 1)
     {
-        printf("\nselect a programm : \n\n1-heavy wash\n2-normal wash \n3-express wash\n\n");
+    printf("\nselect a programm : \n\n1-heavy wash\n2-normal wash \n3-express wash\n\n");
     scanf("%d", &programm);
 
         switch(programm)
@@ -26,38 +32,47 @@ int main()
             break;
         case 3:
             printf("express wash started\n");
-            break;
-
-        default:
-        printf("MAchine didnt start");
-            break;
+            break;  
+        default :
+            printf("ERROR");
+            exit(0);
         }
+
         printf("\n\nWashing is almost over, need additional rinse ? YES -1, NO - 2\n\n");
         scanf("%d", &additional);
+
         if (additional == 1) {
             printf("Additional rinse is added");
             
-        } else {
+        } 
+        else 
+        {
         printf(". . .");
         printf("\nYour clothes is clean!\n");
         printf("\nMachine is OFF\n");
-        return 0;
+        exit(0);
         }
         
         printf("\n\nWashing is over. Turn off the machine? YES -1; NO - 2\n\n");
         scanf("%d", &additional);
-        if (additional == 1) {
+
+        if (additional == 1) 
+        {
         printf("\n\n. . .\n\n");
         printf("\nYour clothes is clean!\n");
         printf("\nMachine is OFF\n");}
-        else {
+        else
+        {
         printf(". . .");
         printf("\nYour clothes is clean!\n");
         printf("\nMachine is still ON\n");
         }
-    } else {
+    } 
+    else
+    {
             printf("\nmachine is off\n");
+            exit(0);
         }
 
-    return 0;
+    return(0);
 }
